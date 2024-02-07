13a5669fdd3f58932e59026daf6e0d11953e510b
Zahiruzzaman Chowdhury <nebrus.gamer@gmail.com>
2024-02-07 17:01:06 +0600

    Q3: Implemented

---
+++ Q3/__init__.py
@@ -0,0 +1,32 @@
+def calc_color() -> int:
+    """Takes input from user and returns the number of boxes
+    required to color it in a way that it makes a star
+
+    Returns:
+        `int`:
+            Number of boxes to color
+    """
+
+    while True:
+        # Take input
+        values = input("Enter two integers in the form `n m`: ")
+        # Integer conversion, unpacking, and type check
+        try:
+            n, m = tuple(map(int, values.split(" ")))
+        except ValueError:
+            continue
+
+        # Range check
+        if n not in range(1, 1_000_000_001):
+            continue
+        if m not in range(1, 1_000_000_001):
+            continue
+
+        # If all checks pass, break out of loop
+        break
+
+    # Return amount of boxes to color
+    return min(n, m) * 4 - 3
+
+
+print("Boxes to colour: ", calc_color())
